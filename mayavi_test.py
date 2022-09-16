c334061839c5247b96c14337d2cfeaa034330dfd
amr62 <amr62@bath.ac.uk>
2022-09-16 15:06:26 +0100

    mayavi is working fine. Have some hardware issues where the rendering is not as good as I want it to be. -next steps: have a pipeline to load different sockets from a list of file_names -fix the rendering issues with mayavi_test.py

--- mayavi_test.py
+++ mayavi_test.py
@@ -1,4 +1,55 @@
 import numpy as np
 from mayavi import mlab
-x, y, z, value = np.random.random((4, 40))
-mlab.points3d(x, y, z, value)
+from stl import mesh
+import os
+import matplotlib.pyplot as plt
+
+file_name = "/home/adwaye/PycharmProjects/hip_shape/data/UCLH-Controls/97940593/Right/97940593_RPEL.stl"
+my_mesh = mesh.Mesh.from_file(file_name)
+# x = my_mesh.x
+# y = my_mesh.y
+# z = my_mesh.z
+# mlab.points3d(x, y, z)
+# mlab.show()
+
+mlab.pipeline.surface(mlab.pipeline.open(file_name))
+mlab.show()
+
+
+import os
+from os.path import join
+
+# Enthought library imports
+from mayavi import mlab
+
+### Download the bunny data, if not already on disk ############################
+if not os.path.exists('bunny.tar.gz'):
+    # Download the data
+    try:
+        from urllib import urlopen
+    except ImportError:
+        from urllib.request import urlopen
+    print("Downloading bunny model, Please Wait (3MB)")
+    opener = urlopen(
+                'http://graphics.stanford.edu/pub/3Dscanrep/bunny.tar.gz')
+    open('bunny.tar.gz', 'wb').write(opener.read())
+
+# Extract the data
+import tarfile
+bunny_tar_file = tarfile.open('bunny.tar.gz')
+try:
+    os.mkdir('bunny_data')
+except:
+    pass
+bunny_tar_file.extractall('bunny_data')
+bunny_tar_file.close()
+
+# Path to the bunny ply file
+bunny_ply_file = join('bunny_data', 'bunny', 'reconstruction', 'bun_zipper.ply')
+
+# Render the bunny ply file
+mlab.pipeline.surface(mlab.pipeline.open(bunny_ply_file))
+mlab.show()
+
+import shutil
+shutil.rmtree('bunny_data')
